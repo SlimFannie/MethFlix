39b3eb825c8b09a906d4f56984086faa687ae074
Fannie Hamel Thibault <90933505+Fannistan@users.noreply.github.com>
2022-11-25 10:53:17 -0500

    25/11/22

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -14,12 +14,18 @@
         <div class="row">
             @if (count($films))
                 @foreach($films as $film)
-                <div class="col-3 pb-2">
-                    <div class="card g-0">
-                        <img src="{{ $film->img }}" class="card-img-top" height="500vh">
-                        <div class="card-body bgcard">
-                            <h5 class="card-title">{{ $film->titre }}</h5>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col-3 pb-3">
+                    <div class="card g-0 h-100 bgcard text-center">
+                        <img src="{{ $film->img }}" class="card-img-top" height="550vh">
+                        <h5 class="card-title pt-3">{{ $film->titre }}</h5>
+                        <div class="card-body collapse" id="resume">
+                            <p class="textcharcoal">{{ $film->resume }}</p>
+                        </div>
+                        <div class="card-footer">
+                            <a href="#" class="btn btn-jaune align-bottom">Buzzant! <i class="fa-solid fa-magnifying-glass"></i></a>
+                            <button class="btn btn-vert" type="button" data-bs-toggle="collapse" data-bs-target="#resume" aria-expanded="false" aria-controls="collapseExample">
+                                Synopsis <i class="fa-solid fa-scroll"></i>
+                            </button>
                         </div>
                     </div>
                 </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,48 @@
 <body>
     <!-- Mettre navbar/header avant le yield -->
     <div class="container-fluid bg">
-        <div class="row">
-            <div class="col-12 text-center bgnav">
+        <div class="row bgnav p-2 mb-3">
+            <div class="col-12 text-center">
                 <h1>Methflix <i class="fa-solid fa-pills"></i></h1>
-                <h5>Plane sur ta soirée</h5>
+                <h4>Plane sur ta soirée</h5>
             </div>
-            @yield('contenu')
+            <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/films">Films</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="/acteurs">Acteurs</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
         </div>
+        @yield('contenu')
     </div>
     <!-- Mettre footer après le yield -->
     <script src="https://kit.fontawesome.com/59d2871224.js" crossorigin="anonymous"></script>
